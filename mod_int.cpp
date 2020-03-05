65e41e320587d5c82ba7403e0ae159d2f1eee1bd
ue-sho <tssu45@gmail.com>
2020-03-05 21:26:22 +0900

    constexprを用いた高速化

--- mod_int.cpp
+++ mod_int.cpp
@@ -1,36 +1,37 @@
-const int mod = 1000000007;
-class mint {
-    ll x;
+constexpr int mod = 1000000007;
+struct mint {
 public:
-    mint(ll x=0) : x((x%mod+mod)%mod) {}
-    mint operator-() const { 
+    ll x;
+
+    constexpr mint(ll x=0) : x((x%mod+mod)%mod) {}
+    constexpr mint operator-() const { 
       return mint(-x);
     }
-    mint& operator+=(const mint a) {
+    constexpr mint& operator+=(const mint a) {
         if ((x += a.x) >= mod) x -= mod;
         return *this;
     }
-    mint& operator-=(const mint a) {
+    constexpr mint& operator-=(const mint a) {
         if ((x += mod-a.x) >= mod) x -= mod;
         return *this;
     }
-    mint& operator*=(const mint a) {
+    constexpr mint& operator*=(const mint a) {
         (x *= a.x) %= mod;
         return *this;
     }
-    mint operator+(const mint a) const {
+    constexpr mint operator+(const mint a) const {
         mint res(*this);
         return res+=a;
     }
-    mint operator-(const mint a) const {
+    constexpr mint operator-(const mint a) const {
         mint res(*this);
         return res-=a;
     }
-    mint operator*(const mint a) const {
+    constexpr mint operator*(const mint a) const {
         mint res(*this);
         return res*=a;
     }
-    mint pow(ll t) const {
+    constexpr mint pow(ll t) const {
         if (!t) return 1;
         mint a = pow(t>>1);
         a *= a;
@@ -39,20 +40,20 @@ public:
     }
 
     // for prime mod
-    mint inv() const {
+    constexpr mint inv() const {
         return pow(mod-2);
     }
-    mint& operator/=(const mint a) {
+    constexpr mint& operator/=(const mint a) {
         return (*this) *= a.inv();
     }
-    mint operator/(const mint a) const {
+    constexpr mint operator/(const mint a) const {
         mint res(*this);
         return res/=a;
     }
 
-    friend ostream& operator<<(ostream& os, const mint& m){
-        os << m.x;
-        retrun os;
+    friend ostream& operator<<(ostream& os, const mint& a){
+        os << a.x;
+        return os;
     }
 };
 

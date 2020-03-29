232e25e71358762d643b122dcc1b5f3031f7038b
ue-sho <tssu45@gmail.com>
2020-03-29 19:09:23 +0900

    見なくてもかけるものをOtherにまとめた

---
+++ Other.cpp
@@ -0,0 +1,56 @@
+//最大公約数
+template <class T>
+T gcd(T a, T b) {
+
+    if(b == 0){
+        return a;
+    }
+    else{
+        gcd(b, a%b);
+    }
+}
+//最小公倍数
+template <class T>
+T lcm(T a, T b){
+
+    T g = gcd(a, b);
+
+    return a / g * b;
+}
+
+//約数列挙
+vector<int> divisor(int n){
+
+    vector<int> ret;
+    for(int i = 1; i * i <= n; ++i) {
+        if(n % i == 0) {
+            ret.push_back(i);
+            if(i * i != n){
+                ret.push_back(n / i);
+            } 
+        }
+    }
+    sort(ret.begin(), ret.end());
+    return ret;
+}
+// 素因数分解
+// pairのfirstに素因数　secondに素因数の数
+template <class T>
+vector<pair<T, int>> factorize(T num){
+
+    vector<pair<T, int>> res;
+    for(T i = 2; i * i <= num; ++i){
+        if(num % i){  //割り切れないなら素因数ではない
+            continue;
+        }
+        res.emplace_back(i, 0);
+        while(num % i == 0){
+            res.back().second++;
+            num /= i;
+        }
+    }
+    if(num != 1){
+        res.emplace_back(num, 1);
+    }
+    return res;
+}
--- warshal_froyd.cpp
+++ warshal_froyd.cpp
@@ -2,60 +2,11 @@ const int MAX = 100000;
 int d[MAX][MAX];
 
 void warshall_floyd(int n) {
-  for (int k = 0; k < n; k++){       // 経由する頂点
-    for (int i = 0; i < n; i++) {    // 始点
-      for (int j = 0; j < n; j++) {  // 終点
+  for (int k = 0; k < n; k++){
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
         d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
       }
     }
   }
-}
-
-//d[i][j] 始点iから終点jまでの最短コストが入る。
-
-int main() {
-
-    //どの経路よりも大きい数字
-    //たどり着けないことを表す
-    constexpr int INF = 1e8;
-
-    //頂点数
-    int V;
-    cin >> V;
-
-    //辺の数
-    int E;
-    cin >> E;
-
-    //隣接行列G
-    //G[i][j]･･･頂点iから頂点jに行くまでの最短コスト
-    vector<vector<int>> G(V, vector<int>(V, INF));
-
-    //自分の頂点へは移動するコストは0なので0にする
-    for (int i = 0; i < V; i++) {
-        G[i][i] = 0;
-    }
-
-    //辺の入力
-    for (int i = 0; i < E; i++) {
-        int u, v, cost;
-        cin >> u >> v >> cost, u--, v--;
-
-        //頂点uからvへの辺のうち、一番小さいコストを使う
-        G[u][v] = min(G[u][v], cost);
-
-        //頂点vからuへの辺のうち、一番小さいコストを使う
-        G[v][u] = min(G[v][u], cost);
-    }
-
-    //ワーシャルフロイド法
-    for (int k = 0; k < V; k++) {
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++) {
-                G[i][j] = min(G[i][j], G[i][k] + G[k][j]);
-            }
-        }
-    }
-
-    return 0;
 }

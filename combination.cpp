2b3fff7867a226f68e41df70fed3a1c1ba0f756a
ue-sho <tssu45@gmail.com>
2020-03-29 18:49:42 +0900

    class化した

--- combination.cpp
+++ combination.cpp
@@ -1,33 +1,29 @@
 // nCr 組み合わせ
-
-const int MAX = 1e5+1; //ここは問題によって変える
-const int mod = 1e9+7
-//  n!  *   (k!)^-1  *  ((n-k)!)^-1
-ll fac[MAX], finv[MAX], inv[MAX];
-
-// テーブルを作る前処理
-// 計算量　O(n)
-void COMinit() {
-    fac[0] = fac[1] = 1;
-    finv[0] = finv[1] = 1;
-    inv[1] = 1;
-    for (int i = 2; i < MAX; ++i){
-        fac[i] = fac[i - 1] * i % mod;
-        inv[i] = mod - inv[mod%i] * (mod / i) % mod;
-        finv[i] = finv[i - 1] * inv[i] % mod;
-    }
-}
-
-// 二項係数計算 nCk
+// 前処理　計算量 O(log n)
 // 計算量　O(1)
-ll Combination(int n, int k){
-    if (n < k){
-        return 0;
+constexpr int mod = 1e9+7;
+class Combination {
+public:
+    vector<ll> fac, finv, inv;
+    Combination(int n) : fac(n), finv(n), inv(n) {
+        fac[0] = fac[1] = 1;
+        finv[0] = finv[1] = 1;
+        inv[1] = 1;
+        for (int i = 2; i < n; ++i){
+            fac[i] = fac[i - 1] * i % mod;
+            inv[i] = mod - inv[mod%i] * (mod / i) % mod;
+            finv[i] = finv[i - 1] * inv[i] % mod;
+        }
     }
-    if (n < 0 || k < 0){
-        return 0;
-    } 
-    return fac[n] * (finv[k] * finv[n - k] % mod) % mod;
-}
+    ll calc(int n, int k){
+        if (n < k){
+            return 0;
+        }
+        if (n < 0 || k < 0){
+            return 0;
+        } 
+        return fac[n] * (finv[k] * finv[n - k] % mod) % mod;
+    }
+};
 
-
+//重複なし組み合わせ　nHk = (n+k-1)C(n-1)

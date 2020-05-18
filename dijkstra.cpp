bbdebd7bb9eedcc097e38475b3902d38f64d3f48
ue-sho <tssu45@gmail.com>
2020-05-18 14:53:27 +0900

    ダイクストラ関数化

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,55 +1,27 @@
-//ダイクストラ
-
-// <最短距離, 頂点の番号>
-//using P = pair<int, int>;
-
-const int INF = 1e9;
-const int MAX_V = 10;
-
 struct edge {
     int to;
     int cost;
 };
 
-int V;
-vector<edge> G[MAX_V];
-int d[MAX_V];
+void dijkstra(const vector<vector<edge>>& edges, vector<int>& distance, int start_point) {
+    distance[start_point] = 0;
 
-void dijkstra(int s) {
-    priority_queue<P, vector<P>, greater<P> > que;
-    fill(d, d+V, INF);
-    d[s] = 0;
-    que.push(P(0, s));
+    // P -> first : 最短距離, second : 頂点の番号
+    priority_queue<P, vector<P>, greater<P>> que;
+    que.emplace(0, start_point);
 
     while (!que.empty()) {
         P p = que.top();
         que.pop();
-        int v = p.second;
-        if (d[v] < p.first) continue;
-
-        for (int i=0; i<G[v].size(); ++i) {
-            edge e = G[v][i];
-            if (d[e.to] > d[v] + e.cost) {
-                d[e.to] = d[v] + e.cost;
-                que.push(P(d[e.to], e.to));
+        int next = p.second;
+        if (distance[next] < p.first){
+            continue;
+        }
+        for(const auto e : edges[p.second]){
+            if(distance[e.to] > distance[next] + e.cost){
+                distance[e.to] = distance[next] + e.cost;
+                que.emplace(distance[e.to], e.to);
             }
         }
-    }
-}
-
-int main() {
-    cin >> V;
-    int E;
-    cin >> E;
-    for (int i=0; i<E; ++i) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        edge e = {b, c};
-        G[a].push_back(e);
-    }
-    dijkstra(0);
-    for (int i=0; i<V; ++i) {
-        if(d[i] != INF)
-            cout << "0から" << i << "までのコスト: " << d[i] << endl;
     }
 }

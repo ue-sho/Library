2237fec315edcc2e80e1636dc8a8bf7f42bb4d95
ue-sho <tssu45@gmail.com>
2020-05-18 14:04:56 +0900

    minimum spaninng tree 追加

--- Other.cpp
+++ Other.cpp
@@ -1,3 +1,33 @@
+// 最小全域木(minimum spaninng tree)　// コストが最小になるような木を作る
+struct edge {
+    int from, to;
+    int cost;
+};
+
+int prim(vector<vector<edge>>& edges, int V) {
+    int res = 0;
+    vector<bool> used(V, false);
+    // P -> first : コスト, second : 行先  // コストが小さい順に出しいく
+    priority_queue<P, vector<P>, greater<>> priorityQueue;
+    priorityQueue.emplace(0, 0);
+    while (!priorityQueue.empty()) {
+        P p = priorityQueue.top();
+        priorityQueue.pop();
+        if(used[p.second]){
+            continue;
+        }
+        used[p.second] = true;
+        res += p.first;
+        for(edge<T> &e : edges[p.second]){
+            if(!used[e.to]){
+                priorityQueue.push({e.cost, e.to});
+            }
+        }
+    }
+    return res;
+}
+
+
 // 逆元
 int mod_inv(int a){
     ll b = mod, u = 1, v = 0;
@@ -15,6 +45,17 @@ int mod_inv(int a){
     return u;
 }
 
+// ワーシャルフロイド
+void warshall_floyd(int n) {
+  for (int k = 0; k < n; k++){
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+      }
+    }
+  }
+}
+
 //最大公約数
 template <class T>
 T gcd(T a, T b) {
@@ -26,6 +67,7 @@ T gcd(T a, T b) {
         gcd(b, a%b);
     }
 }
+
 //最小公倍数
 template <class T>
 T lcm(T a, T b){
@@ -71,15 +113,4 @@ vector<pair<T, int>> factorize(T num){
         res.emplace_back(num, 1);
     }
     return res;
-}
-
-// ワーシャルフロイド
-void warshall_floyd(int n) {
-  for (int k = 0; k < n; k++){
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
-      }
-    }
-  }
 }
